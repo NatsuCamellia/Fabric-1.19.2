ff6e9512ee3b213583d1c14ee490f79f37639d25
NatsuCamellia <natsucamellia@gmail.com>
2023-02-04 22:17:23 +0800

    Initial commit

---
+++ src/main/java/net/natsucamellia/tanzanite/Tanzanite.java
@@ -0,0 +1,14 @@
+package net.natsucamellia.tanzanite;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tanzanite implements ModInitializer {
+	public static final String MOD_ID = "tanzanite";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/natsucamellia/tanzanite/TanzaniteClient.java
@@ -0,0 +1,10 @@
+package net.natsucamellia.tanzanite;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TanzaniteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
