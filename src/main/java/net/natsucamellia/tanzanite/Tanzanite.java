fccaefae7ea9b2626251931f6b2cacc9b2b813ef
NatsuCamellia <natsucamellia@gmail.com>
2023-02-04 23:17:32 +0800

    Add custom blocks

--- src/main/java/net/natsucamellia/tanzanite/Tanzanite.java
+++ src/main/java/net/natsucamellia/tanzanite/Tanzanite.java
@@ -1,6 +1,7 @@
 package net.natsucamellia.tanzanite;
 
 import net.fabricmc.api.ModInitializer;
+import net.natsucamellia.tanzanite.block.ModBlocks;
 import net.natsucamellia.tanzanite.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,5 +13,6 @@ public class Tanzanite implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
